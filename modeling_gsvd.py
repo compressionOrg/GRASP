addc2a400c493328acaccde03dd4a6f779c47cf5
Kainan-Liu <kliu341@connect.hkust-gz.edu.cn>
2024-10-09 17:53:36 +0800

    fix bugs

--- modeling_gsvd.py
+++ modeling_gsvd.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from typing import Literal, Optional
-from transformers import AutoModelForCausalLM
 
 
 class GSVDLayer(nn.Module):
