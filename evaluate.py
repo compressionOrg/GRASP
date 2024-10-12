d84348a3309bca3f7696f10817eb2fbb0bca4cbe
Kainan-Liu <kliu341@connect.hkust-gz.edu.cn>
2024-10-12 22:39:26 +0800

    commit

--- evaluate.py
+++ evaluate.py
@@ -4,31 +4,31 @@ from modeling_gsvd import GSVDModel
 from dataset.loader import get_calibration_dataloader
 
 
-def quick_test():
+def quick_test(model_name: str):
     gsvd_model = GSVDModel(model=model)
     gsvd_model.compress_block(layers_id = [i for i in range(1, 22)], target_layer_types=["mlp.down_proj", "mlp.up_proj", "self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj"])
     indices_dict = gsvd_model.naive_svd_selection(compression_ratio=0.2)
     gsvd_model.compile_gsvd_model(indices_dict, verbose=True)
 
-    result = evaluate_model(gsvd_model.model, tokenizer, model_name="qwen", tasks="", eval_ppl="wikitext2")
+    result = evaluate_model(gsvd_model.model, tokenizer, model_name=model_name, tasks="", eval_ppl="wikitext2")
 
 
-def main_test():
+def main_test(model_name: str):
     import gsvd
     gsvd_model = gsvd.compress(
         model=model,
         calibration_dataloader=calibration_dataloader,
         mode="recursive",
-        layers_id=[i for i in range(1, 31)],
+        layers_id=[i for i in range(1, 22)],
         target_layer_types=["mlp.down_proj", "mlp.up_proj", "self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj"],
         compression_ratio=0.2,
         metric="taylor",
         verbose=True
     )
-    result = evaluate_model(gsvd_model.model, tokenizer, model_name="qwen", tasks="", eval_ppl="wikitext2")
+    result = evaluate_model(gsvd_model.model, tokenizer, model_name=model_name, tasks="", eval_ppl="wikitext2")
 
-def original_test():
-    result = evaluate_model(model, tokenizer, model_name="qwen", tasks="", eval_ppl="wikitext2")
+def original_test(model_name: str):
+    result = evaluate_model(model, tokenizer, model_name=model_name, tasks="", eval_ppl="wikitext2")
 
 
 
@@ -40,4 +40,6 @@ if __name__ == "__main__":
     # tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-0.5B-Instruct')
     calibration_dataloader = get_calibration_dataloader(dataset_name="wikitext2", tokenizer=tokenizer)
 
-    main_test()
+    main_test(model_name="llama")
+
+    
