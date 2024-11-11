3afe2f290bbf9329d89cd5e5e3b4287ea9831892
Kainan-Liu <kliu341@connect.hkust-gz.edu.cn>
2024-11-11 19:50:23 +0800

    fix device bugs of evaluating scripts

--- evaluate_gsvd.py
+++ evaluate_gsvd.py
@@ -27,10 +27,11 @@ class EvalLM(BaseLM):
         # assert isinstance(device, str)
         assert isinstance(batch_size, int)
 
-        self._device = torch.device(device)
         # self.model = model.to(self.device)
         self.model = model
         self.model.eval()
+
+        self._device = self.model.device 
 
         self.tokenizer = tokenizer
 
@@ -164,7 +165,7 @@ def evaluate_model(
             nlls = []
 
             for i in tqdm(range(nsamples)):
-                batch = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)].to(lm.model.device)
+                batch = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)].to(lm.device)
                 if is_peft_model:
                     outputs = lm.model.model.model(batch)
                     hidden_states = outputs[0]  # .to(lm.model.lm_head.weight.device)
@@ -174,7 +175,7 @@ def evaluate_model(
                     hidden_states = outputs[0]  # .to(lm.model.lm_head.weight.device)
                     logits = lm.model.lm_head(hidden_states)  # .contiguous()
                 shift_logits = logits[:, :-1, :]  # .contiguous()
-                shift_labels = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)][:, 1:].to(lm.model.device)
+                shift_labels = testenc[:, (i * lm.seqlen) : ((i + 1) * lm.seqlen)][:, 1:].to(lm.device)
                 loss_fct = nn.CrossEntropyLoss()
                 loss = loss_fct(
                     shift_logits.view(-1, shift_logits.size(-1)),
